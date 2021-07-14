1df8765cde3aee6b1925001d886392ccb6fec3d1
andreamon <andrea14_06_2@hotmail.com>
2021-07-14 13:46:22 -0300

    add Input Component

--- src/Input.js
+++ src/Input.js
@@ -1,20 +1,38 @@
-import { useState } from "react";
-
+// import { useState } from "react";
 function Input(props) {
-  const [inputVal, setInputVal] = useState(0);
-  function reset() {
-    setInputVal(0);
-  }
-  function seeResult(e) {
-    props.valRes = e.target.value;
-    console.log(e.target.value);
-  }
-  return (
-    <>
-      <input onChange={seeResult} type="number" defaultValue={inputVal} />
-      <button onClick={reset}>Clear</button>
-    </>
-  );
+	// const [inputComponent, setInputComponent] = useState(0);
+	const { id } = props;
+
+	// const clear = () => {
+	// 	document.getElementById(id).value = 0;
+	// 	setInputComponent(0);
+	// };
+
+	// const checkTest = (e) => {
+	// 	let checkComp = isNaN(e.target.value);
+	// 	console.log(checkComp);
+	// 	console.log(e.target.id);
+	// 	let inputId = e.target.id;
+	// 	if (!checkComp) {
+	// 		setInputComponent(e.target.value);
+	// 	} else {
+	// 		document.getElementById(inputId).value = "";
+	// 		setInputComponent(0);
+	// 	}
+	// };
+
+	return (
+		<div className="main-input">
+			<input id={id} onChange={props.checkType} />
+			<button
+				onClick={() => {
+					props.clear(id);
+				}}
+			>
+				Clear
+			</button>
+		</div>
+	);
 }
 
 export default Input;
